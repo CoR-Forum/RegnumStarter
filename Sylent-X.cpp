ee07838239fa0a26736a203c49ed4a00ef5d3bb1
manuoderso <olthoffmanuel@gmail.com>
2024-10-06 17:12:01 +0200

    Add renderer backends for DirectX9 and Win32 and GUI

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -4,16 +4,33 @@
 #include <objbase.h>
 #include <tlhelp32.h>
 #include <wininet.h>
+#include <tchar.h> // Add this header for TEXT macro
 #include "Utils.h"
 #include "Updater.cpp"
 #include "Logger.cpp"
 #include "ApiHandler.cpp"
 #include "Windows.cpp"
+#include "imgui.h"
+#include "imgui_impl_dx9.h"
+#include "imgui_impl_win32.h"
+#include <d3d9.h>
 
 #pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "urlmon.lib")
+#pragma comment(lib, "dwmapi.lib")
 
 #define WM_CLOSE_REGISTRATION_WINDOW (WM_USER + 1)
+
+static LPDIRECT3D9              g_pD3D = nullptr;
+static LPDIRECT3DDEVICE9        g_pd3dDevice = nullptr;
+static bool                     g_DeviceLost = false;
+static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
+static D3DPRESENT_PARAMETERS    g_d3dpp = {};
+
+bool CreateDeviceD3D(HWND hWnd);
+void CleanupDeviceD3D();
+void ResetDevice();
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 std::atomic<bool> isWriting(false);
 std::thread memoryThread;
@@ -25,6 +42,39 @@ void ContinuousMemoryWrite(const std::string& option) {
     }
 }
 
+// Function to initialize DirectX
+bool InitDirectX(HWND hwnd) {
+    if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL) {
+        return false;
+    }
+
+    ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+    g_d3dpp.Windowed = TRUE;
+    g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+    g_d3dpp.EnableAutoDepthStencil = TRUE;
+    g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+    g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
+
+    if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hwnd, 
+                             D3DCREATE_HARDWARE_VERTEXPROCESSING, 
+                             &g_d3dpp, &g_pd3dDevice) < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+// Function to reset the Direct3D device
+void ResetDevice() {
+    ImGui_ImplDX9_InvalidateDeviceObjects();
+    HRESULT hr = g_pd3dDevice->Reset(&g_d3dpp);
+    if (hr == D3DERR_INVALIDCALL) {
+        IM_ASSERT(0);
+    }
+    ImGui_ImplDX9_CreateDeviceObjects();
+}
+
 // Constants
 const std::string currentVersion = "0.1.50"; // Current version of the application
 const char* appName = "Sylent-X";
@@ -33,48 +83,127 @@ HANDLE hProcess = nullptr; // Handle to the target process (ROClientGame.exe)
 DWORD pid; // Process ID of the target process
 
 int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    hInstanceGlobal = hInstance; // Assign to global variable
-
     Log("Sylent-X " + currentVersion + ". Made with hate in Germany.");
 
     LoadSettings();
 
     // Register and create the main window
-    if (!RegisterMainWindowClass(hInstance, appName)) {
-        MessageBox(NULL, "Failed to register window class.", "Error", MB_ICONERROR);
-        return 0;
-    }
+  WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
+  ::RegisterClassExW(&wc);
+  HWND hwnd = CreateWindowEx(WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_NOACTIVATE, _T("ImGui Example"), NULL, WS_POPUP, 0, 0, 1980, 1080, NULL, NULL, wc.hInstance, NULL);
+  SetLayeredWindowAttributes(hwnd, RGB(0, 0, 0), 0, LWA_COLORKEY);
+ 
+  if (!CreateDeviceD3D(hwnd))
+  {
+      CleanupDeviceD3D();
+      ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
+      return 1;
+  }
 
-    HWND hwnd = CreateMainWindow(hInstance, appName, currentVersion, nCmdShow);
-    if (hwnd == NULL) {
-        MessageBox(NULL, "Failed to create window.", "Error", MB_ICONERROR);
-        return 0;
-    }
+  ::ShowWindow(hwnd, SW_SHOWDEFAULT);
+  ::UpdateWindow(hwnd);
 
-    // Set the global keyboard hook
-    hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, hInstance, 0);
-    if (!hKeyboardHook) {
-        MessageBox(NULL, "Failed to set keyboard hook.", "Error", MB_ICONERROR);
-        return 0;
-    }
 
-    if (!Login(login, password)) {
-        Log("Login failed.");
-        OpenLoginWindow();
-    }
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO& io = ImGui::GetIO(); (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      
 
-    // Post custom message to start selfupdate
-    PostMessage(hwnd, WM_START_SELF_UPDATE, 0, 0);
+  ImGui::StyleColorsDark();
 
-    // Message loop
-    MSG msg = { 0 };
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
+  ImGui_ImplWin32_Init(hwnd);
+  ImGui_ImplDX9_Init(g_pd3dDevice);
+
+
+  
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+ 
+  bool done = false;
+  while (!done)
+  { 
+      MSG msg;
+      while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
+      {
+          ::TranslateMessage(&msg);
+          ::DispatchMessage(&msg);
+          if (msg.message == WM_QUIT)
+              done = true;
+      }
+      if (done)
+          break;
+
+      if (g_DeviceLost)
+      {
+          HRESULT hr = g_pd3dDevice->TestCooperativeLevel();
+          if (hr == D3DERR_DEVICELOST)
+          {
+              ::Sleep(10);
+              continue;
+          }
+          if (hr == D3DERR_DEVICENOTRESET)
+              ResetDevice();
+          g_DeviceLost = false;
+      }
+      if (g_ResizeWidth != 0 && g_ResizeHeight != 0)
+      {
+          g_d3dpp.BackBufferWidth = g_ResizeWidth;
+          g_d3dpp.BackBufferHeight = g_ResizeHeight;
+          g_ResizeWidth = g_ResizeHeight = 0;
+          ResetDevice();
+      }
+      ImGui_ImplDX9_NewFrame();
+      ImGui_ImplWin32_NewFrame();
+      ImGui::NewFrame();
+
+      if (show_demo_window)
+
+      {
+        ImGui::Begin("Welcome, Sylent-X User!");         
+        ImGui::SetWindowSize(ImVec2(500, 300));
+        ImGui::End();
+        ImGui::Begin("Welcome, Test!");         
+        ImGui::SetWindowSize(ImVec2(500, 300));
+        ImGui::End();
+        ImGui::Begin("Welcome, Test2!");         
+        ImGui::SetWindowSize(ImVec2(500, 300));
+        ImGui::End();
+      }
+
+
+
+      // Rendering
+      ImGui::EndFrame();
+      g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, FALSE);
+      g_pd3dDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, FALSE);
+      g_pd3dDevice->SetRenderState(D3DRS_SCISSORTESTENABLE, FALSE);
+      D3DCOLOR clear_col_dx = D3DCOLOR_RGBA((int)(clear_color.x * clear_color.w * 0.0f), (int)(clear_color.y * clear_color.w * 0.0f), (int)(clear_color.z * clear_color.w * 0.0f), (int)(clear_color.w * 0.0f));
+      g_pd3dDevice->Clear(0, nullptr, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, clear_col_dx, 1.0f, 0);
+      if (g_pd3dDevice->BeginScene() >= 0)
+      {
+          ImGui::Render();
+          ImGui_ImplDX9_RenderDrawData(ImGui::GetDrawData());
+          g_pd3dDevice->EndScene();
+      }
+      HRESULT result = g_pd3dDevice->Present(nullptr, nullptr, nullptr, nullptr);
+      if (result == D3DERR_DEVICELOST)
+          g_DeviceLost = true;
+  }
+
+  
+  ImGui_ImplDX9_Shutdown();
+  ImGui_ImplWin32_Shutdown();
+  ImGui::DestroyContext();
+
+  CleanupDeviceD3D();
+  ::DestroyWindow(hwnd);
+  ::UnregisterClassW(wc.lpszClassName, wc.hInstance);
 
     Log("Sylent-X exiting");
-    return (int)msg.wParam;
+    return 0;
 }
 
 // Function to get the base address of a module
@@ -82,25 +211,18 @@ uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName) {
     uintptr_t modBaseAddr = 0;
     HANDLE hSnap = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
     if (hSnap != INVALID_HANDLE_VALUE) {
-        // LogDebug("Process snapshot created for process ID: " + std::to_string(procId));
         MODULEENTRY32 modEntry;
         modEntry.dwSize = sizeof(modEntry);
         if (Module32First(hSnap, &modEntry)) {
             do {
                 wchar_t wModuleName[MAX_PATH];
                 MultiByteToWideChar(CP_ACP, 0, modEntry.szModule, -1, wModuleName, MAX_PATH);
-                // LogDebug("Checking module: " + std::string(modEntry.szModule));
                 if (!_wcsicmp(wModuleName, modName)) {
                     modBaseAddr = (uintptr_t)modEntry.modBaseAddr;
-                    // LogDebug("Module found: " + std::string(modEntry.szModule) + 
-                    //     " at address: " + std::to_string(modBaseAddr) + 
-                    //     " in process ID: " + std::to_string(procId));
                     break;
                 }
             } while (Module32Next(hSnap, &modEntry));
         }
-    } else {
-        LogDebug("Failed to create process snapshot for process ID: " + std::to_string(procId));
     }
     CloseHandle(hSnap);
     return modBaseAddr;
@@ -115,7 +237,6 @@ DWORD GetProcessIdByName(const std::wstring& processName) {
         pe32.dwSize = sizeof(PROCESSENTRY32);
         if (Process32First(hSnap, &pe32)) {
             do {
-                // Convert pe32.szExeFile to a wide string
                 std::wstring exeFile(pe32.szExeFile, pe32.szExeFile + strlen(pe32.szExeFile));
                 if (!_wcsicmp(exeFile.c_str(), processName.c_str())) {
                     processId = pe32.th32ProcessID;
@@ -143,45 +264,33 @@ bool Memory::WriteFloat(uintptr_t address, float value) {
 }
 
 void MemoryManipulation(const std::string& option) {
-    // LogDebug("Performing memory manipulation for " + option);
-
-    // Get process ID for ROClientGame.exe
     pid = GetProcessIdByName(L"ROClientGame.exe");
     if (pid == 0) {
         Log("Failed to find ROClientGame.exe process");
         MessageBox(NULL, "Failed to find ROClientGame.exe process.", "Error", MB_ICONERROR);
         return;
     }
-    // LogDebug("Process ID for ROClientGame.exe: " + std::to_string(pid));
 
-    // Open the process with full access
     hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
     if (!hProcess) {
         Log("Failed to open ROClientGame.exe process. Error code: " + std::to_string(GetLastError()));
         MessageBox(NULL, "Failed to open ROClientGame.exe process.", "Error", MB_ICONERROR);
         return;
     }
-    // LogDebug("Successfully opened process with ID: " + std::to_string(pid));
 
-    // Get the base address of ROClientGame.exe
     uintptr_t baseAddress = GetModuleBaseAddress(pid, L"ROClientGame.exe");
     if (baseAddress == 0) {
         Log("Failed to get the base address of ROClientGame.exe");
         CloseHandle(hProcess);
         return;
     }
-    // LogDebug("Base address of ROClientGame.exe: " + std::to_string(baseAddress));
 
-    // Use the fetched pointers
     for (const auto& pointer : pointers) {
         if (pointer.name == option) {
             uintptr_t optionPointer = baseAddress + pointer.address;
-            // LogDebug(option + " pointer address: " + std::to_string(optionPointer));
-
             uintptr_t finalAddress = optionPointer;
             SIZE_T bytesRead;
 
-            // Apply each offset sequentially
             for (size_t i = 0; i < pointer.offsets.size(); ++i) {
                 if (ReadProcessMemory(hProcess, (LPCVOID)finalAddress, &finalAddress, sizeof(finalAddress), &bytesRead)) {
                     if (bytesRead != sizeof(finalAddress)) {
@@ -195,13 +304,9 @@ void MemoryManipulation(const std::string& option) {
                 }
             }
 
-            // Convert finalAddress to hexadecimal string
             std::stringstream ss;
             ss << std::hex << std::uppercase << finalAddress;
             std::string finalAddressHex = ss.str();
-
-            // Log the final address and value being written
-            // LogDebug("Final address: " + finalAddressHex);
 
             float newValue = 0.0f;
 
@@ -219,9 +324,6 @@ void MemoryManipulation(const std::string& option) {
                 newValue = 9.219422856E-41f;
             }
 
-
-            // LogDebug("Writing value: " + std::to_string(newValue) + " to address: " + finalAddressHex);
-
             if (WriteProcessMemory(hProcess, (LPVOID)finalAddress, &newValue, sizeof(newValue), NULL)) {
                 // LogDebug("Successfully wrote new " + option + " value: " + std::to_string(newValue));
             } else {
@@ -230,7 +332,59 @@ void MemoryManipulation(const std::string& option) {
         }
     }
 
-    // Close the process handle
     CloseHandle(hProcess);
-    // LogDebug("Memory manipulation completed");
+}
+
+void CleanupDeviceD3D()
+{
+    if (g_pd3dDevice) { g_pd3dDevice->Release(); g_pd3dDevice = nullptr; }
+    if (g_pD3D) { g_pD3D->Release(); g_pD3D = nullptr; }
+}
+
+
+
+extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
+        return true;
+
+    switch (msg)
+    {
+    case WM_SIZE:
+        if (wParam == SIZE_MINIMIZED)
+            return 0;
+        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeHeight = (UINT)HIWORD(lParam);
+        return 0;
+    case WM_SYSCOMMAND:
+        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+            return 0;
+        break;
+    case WM_DESTROY:
+        ::PostQuitMessage(0);
+        return 0;
+    }
+    return ::DefWindowProcW(hWnd, msg, wParam, lParam);
+}
+
+bool CreateDeviceD3D(HWND hWnd)
+
+{
+    if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == nullptr)
+        return false;
+
+    ZeroMemory(&g_d3dpp, sizeof(g_d3dpp));
+    g_d3dpp.Windowed = TRUE;
+    g_d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+    g_d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+    g_d3dpp.EnableAutoDepthStencil = TRUE;
+    g_d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
+    g_d3dpp.PresentationInterval = D3DPRESENT_INTERVAL_ONE;          
+    if (g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3dpp, &g_pd3dDevice) < 0)
+        return false;
+
+    return true;
 }
