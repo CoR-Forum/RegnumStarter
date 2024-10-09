71ac87b012c44decc3f7c2eb563d87cafc5197f6
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-09 17:18:38 +0200

    Refactor credits in Sylent-X.cpp

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -399,9 +399,9 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
             ImGui::Text("Sylent-X %s", currentVersion.c_str());
             ImGui::Text("This software is provided as-is without any warranty. Use at your own risk.");
-            ImGui::Text("Made with hate in Germany by Francis, Shaiko and Manu.");
+            ImGui::Text("Made with hate in Germany by AdrianWho, Manu and Francis");
             ImGui::Text("Special thanks to the Champions of Regnum community for their support and feedback.");
-            ImGui::Text("Big shoutout to Adrian Lastres.");
+            ImGui::Text("Big shoutout to Adrian Lastres. You're the best!");
             ImGui::End();
         }
 
