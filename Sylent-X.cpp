5a66ef7c9c6a0dc8144e059280535a0abecd1b04
Joshua Treudler <joshua@treudler.net>
2024-10-09 21:50:58 +0200

    Refactor table size in DisplayUsersTable function

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -731,7 +731,7 @@ void DisplayUsersTable() {
     }
 
     // Begin the ImGui table with a maximum height
-    ImGui::BeginChild("UsersTableChild", ImVec2(0, 300), true, ImGuiWindowFlags_AlwaysVerticalScrollbar);
+    ImGui::BeginChild("UsersTableChild", ImVec2(800, 600), true, ImGuiWindowFlags_AlwaysVerticalScrollbar);
     ImGui::BeginTable("AllUsersTable", 6, ImGuiTableFlags_Borders | ImGuiTableFlags_RowBg | ImGuiTableFlags_Resizable);
         ImGui::TableSetupColumn("ID", ImGuiTableColumnFlags_WidthFixed, 20.0f);
         ImGui::TableSetupColumn("Username", ImGuiTableColumnFlags_WidthFixed, 80.0f);
