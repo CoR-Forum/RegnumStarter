8a4f7d65aa3ac8c978d225e0e7aedd9ec5b819dd
Joshua Treudler <joshua@treudler.net>
2024-10-09 21:40:21 +0200

    Revert "move stuff from the admin UI around"
    This reverts commit 7035cb1cce0410d20086a10193843d6b21de3cf6.

--- ui/windows/admin/AdminWindow.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADMIN_WINDOW_H
-#define ADMIN_WINDOW_H
-
-void ShowAdminWindow(bool* show_admin_window);
-
-#endif // ADMIN_WINDOW_H
--- ui/windows/admin/sub/UsersTable.h
+++
@@ -1,6 +0,0 @@
-#ifndef USERS_TABLE_H
-#define USERS_TABLE_H
-
-void DisplayUsersTable();
-
-#endif // USERS_TABLE_H
