a549064cb0a039649f283b1cb0aba0c74651f2c5
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-18 15:07:23 +0200

    Refactor chrono.cpp: Add function to uncheck moonwalk option after delay

--- includes/chrono/chrono.cpp
+++ includes/chrono/chrono.cpp
@@ -6,3 +6,8 @@ void UncheckFakelagAfterDelay(bool& optionFakelag) {
     std::this_thread::sleep_for(std::chrono::seconds(2));
     optionFakelag = false;
 }
+
+void UncheckMoonwalkAfterDelay(bool& optionMoonwalk) {
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    optionMoonwalk = false;
+}
