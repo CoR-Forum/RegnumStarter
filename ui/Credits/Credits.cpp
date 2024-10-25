20390afe24112a236f4da32abe92622748a4964c
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-26 01:04:45 +0200

    Add Credits window to display information about the software and its creators

---
+++ ui/Credits/Credits.cpp
@@ -0,0 +1,10 @@
+#include "Credits.h"
+
+void ShowCreditsWindow(bool& show_info_window) {
+    if (show_info_window) {
+        ImGui::Text("This software is provided as-is without any warranty. Use at your own risk.");
+        ImGui::Text("Made with hate in Germany by AdrianWho, Manu and Francis");
+        ImGui::Text("Special thanks to the Champions of Regnum community for their support and feedback.");
+        ImGui::Text("Big shoutout to Adrian Lastres. You're the best!");
+    }
+}
