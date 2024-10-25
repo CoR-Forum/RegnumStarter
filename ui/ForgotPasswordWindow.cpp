376f178ee58f20c30d9026d0b4e7b2b36542cf42
Joshua Treudler <joshua@treudler.net>
2024-10-25 22:40:49 +0200

    doc: ForgotPasswordWindow

--- ui/ForgotPasswordWindow.cpp
+++ ui/ForgotPasswordWindow.cpp
@@ -2,14 +2,22 @@
 #include "../libs/imgui/imgui.h"
 #include <windows.h>
 
-// Function to handle the Forgot Password Window
+/**
+ * @brief Displays the "Forgot Password" window and handles user interactions.
+ *
+ * This function creates and manages the "Forgot Password" window using ImGui. It allows users to enter their email address
+ * to request a password reset link. The window provides options to request a password reset, indicate that the user already
+ * has a reset token, or return to the login window.
+ *
+ * @param show_forgot_password_window A reference to a boolean that controls the visibility of the "Forgot Password" window.
+ * @param show_password_reset_window A reference to a boolean that controls the visibility of the "Password Reset" window.
+ * @param show_login_window A reference to a boolean that controls the visibility of the "Login" window.
+ */
 void ShowForgotPasswordWindow(bool& show_forgot_password_window, bool& show_password_reset_window, bool& show_login_window) {
     static bool forgotpassWindowIsOpen = true;
 
-    // Begin the ImGui window
     ImGui::Begin("Forgot Password", &forgotpassWindowIsOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
 
-    // If the window is closed, save settings and quit
     if (!forgotpassWindowIsOpen) {
         SaveSettings();
         PostQuitMessage(0);
@@ -21,7 +29,6 @@ void ShowForgotPasswordWindow(bool& show_forgot_password_window, bool& show_pass
     
     ImGui::InputTextWithHint("##Email", "E-Mail Address", forgotPasswordEmail, IM_ARRAYSIZE(forgotPasswordEmail));
 
-    // Function to handle the submit button click
     auto handleSubmit = [&]() {
         if (ResetPasswordRequest(forgotPasswordEmail)) {
             show_forgot_password_window = false;
@@ -49,6 +56,5 @@ void ShowForgotPasswordWindow(bool& show_forgot_password_window, bool& show_pass
         show_login_window = true;
     }
 
-    // End the ImGui window
     ImGui::End();
 }
--- ui/ForgotPasswordWindow.h
+++ ui/ForgotPasswordWindow.h
@@ -8,6 +8,16 @@
 static char forgotPasswordEmail[128] = "";
 
 // Function to display the Forgot Password window
+/**
+ * @brief Displays the Forgot Password window and manages the visibility of related windows.
+ * 
+ * This function is responsible for showing the Forgot Password window and updating the visibility
+ * states of the password reset and login windows based on user interactions.
+ * 
+ * @param show_forgot_password_window A reference to a boolean that determines whether the Forgot Password window is shown.
+ * @param show_password_reset_window A reference to a boolean that determines whether the Password Reset window is shown.
+ * @param show_login_window A reference to a boolean that determines whether the Login window is shown.
+ */
 void ShowForgotPasswordWindow(bool& show_forgot_password_window, bool& show_password_reset_window, bool& show_login_window);
 
 // Function to save settings (externally defined)
