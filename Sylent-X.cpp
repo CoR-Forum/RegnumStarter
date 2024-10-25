acf72f1afb254266702ad0cd0ff5a5275a8af10f
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-26 00:19:01 +0200

    Refactor include path for Feedback.cpp

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -22,6 +22,7 @@
 #include "ui/login/LoginWindow.cpp"
 #include "ui/login/register/RegisterWindow.cpp"
 #include "ui/RegnumStarter/RegnumStarter.cpp"
+#include "ui/Feedback/Feedback.cpp"
 #include "ui/WindowStates.h"
 
 
@@ -458,33 +459,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
 
             } else if (show_feedback_window) {
 
-                    static int feedbackType = 0;
-                    static bool feedback_includeLogfile = true;
-                    const char* feedbackTypes[] = { "Suggestion", "Bug Report", "Other" };
-                    static char feedbackText[1024] = "";
-                    static std::string feedbackMessage = "";
-
-                    ImGui::Combo("Type", &feedbackType, feedbackTypes, IM_ARRAYSIZE(feedbackTypes));
-
-                    ImGui::SameLine();
-                    ImGui::Checkbox("Include Log File", &feedback_includeLogfile);
-
-                    ImGui::InputTextMultiline("Feedback", feedbackText, IM_ARRAYSIZE(feedbackText), ImVec2(480, ImGui::GetTextLineHeight() * 10));
-
-
-                    if (ImGui::Button("Submit")) {
-                        try {
-                            SendFeedback(feedbackTypes[feedbackType], feedbackText, feedback_includeLogfile);
-                            feedbackMessage = "Feedback sent successfully!";
-                            feedbackText[0] = '\0'; // Clear the feedback text
-                        } catch (const std::exception& e) {
-                            feedbackMessage = "Failed to send feedback: " + std::string(e.what());
-                        }
-                    }
-
-                    if (!feedbackMessage.empty()) {
-                        ImGui::Text("%s", feedbackMessage.c_str());
-                    }
+                ShowFeedbackWindow(show_feedback_window);
 
             } else if (show_license_window) {
                     static char licenseKey[128] = "";
