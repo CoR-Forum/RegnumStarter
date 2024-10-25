86c3ff3bf0e53783ff7adcd451e1e543b94a782e
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-25 21:30:52 +0200

    Refactor code to include Login window in Sylent-X.cpp

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -20,6 +20,7 @@
 #include "ui/ForgotPasswordWindow.cpp"
 #include "ui/PasswordResetWindow.cpp"
 #include "ui/Rainbow/UpdateRainbowColor.cpp"
+#include "ui/Login/Login.cpp"
 
 #pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "urlmon.lib")
@@ -216,65 +217,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
         
         if (g_ShowUI) {
             if (show_login_window) {
-                static bool settingsWindowIsOpen = true;
-                ImGui::Begin("Login", &settingsWindowIsOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
-                
-                if (!settingsWindowIsOpen) {
-                SaveSettings();
-                PostQuitMessage(0);
-                }
-
-                ImGui::InputText("Username", username, IM_ARRAYSIZE(username));
-                ImGui::InputText("Password", password, IM_ARRAYSIZE(password), ImGuiInputTextFlags_Password);
-                
-                if (ImGui::Button("Login")) {
-                    show_loading_screen = true;
-                    statusMessage = "Logging in...";
-                    loginSuccess = false;
-                    show_login_window = false; // Hide the login window
-
-                    std::thread loginThread([&]() {
-                        for (int i = 0; i <= 100; ++i) {
-                            std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simulate progress over 1s
-                            UpdateProgressBar(i / 100.0f);
-                        }
-
-                        loginSuccess = Login(username, password);
-                        if (loginSuccess) {
-                            Log("Login successful");
-                            SaveLoginCredentials(username, password);
-                            show_main_window = true;
-
-                            // Reapply color settings after manual login
-                            ImGui::GetStyle().Colors[ImGuiCol_Text] = textColor;
-                            ImGui::GetStyle().Colors[ImGuiCol_TextDisabled] = textColor;
-                        } else {
-                            Log("Login failed");
-                            show_login_window = true; // Show the login window again if login fails
-                        }
-                        show_loading_screen = false;
-                    });
-
-                    loginThread.detach();
-                }
-
-                if (loginSuccess) {
-                    ImGui::Text("Login successful");
-                } else {
-                    ImGui::Text("Login failed");
-                }
-
-                if (ImGui::Button("Register")) {
-                    show_login_window = false;
-                    show_register_window = true;
-                }
-
-                if (ImGui::Button("Forgot Password")) {
-                    show_login_window = false;
-                    show_forgot_password_window = true;
-                }
-
-                ImGui::End();
+                ShowLoginWindow(show_login_window, show_loading_screen, statusMessage, loginSuccess, show_main_window, textColor);
             }
 
             if (show_register_window) {
