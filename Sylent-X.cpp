cfe7fcada172ddc0f2bc389bb14c7f9b9622756f
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-26 01:17:14 +0200

    Refactor include paths and add View window

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -27,6 +27,7 @@
 #include "ui/Movement/Movement.cpp"
 #include "ui/Credits/Credits.cpp"
 #include "ui/Player/Player.cpp"
+#include "ui/View/View.cpp"
 #include "ui/WindowStates.h"
 
 
@@ -57,16 +58,6 @@ std::vector<Pointer> pointers;
 
 const std::string regnumLoginUser = "username";
 const std::string regnumLoginPassword = "password";
-
-// Function to get the key name from the virtual key code
-std::string GetKeyName(int virtualKey) {
-    UINT scanCode = MapVirtualKey(virtualKey, MAPVK_VK_TO_VSC);
-    char keyName[128];
-    if (GetKeyNameText(scanCode << 16, keyName, sizeof(keyName)) > 0) {
-        return std::string(keyName);
-    }
-    return "Unknown";
-}
 
 // Function to check if a hotkey is pressed
 bool IsHotkeyPressed(int hotkey) {
@@ -471,64 +462,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                 ShowCreditsWindow(show_info_window);  
 
             } else if (show_view_window) {
-                static float zoomValue = 15.0f; // Default zoom value
-                static bool prevZoomState = false; // Track previous state of the checkbox
-
-                ImGui::Checkbox("Enable Zoom", &optionZoom);
-                if (optionZoom) {
-                    ImGui::SameLine();
-                    if (ImGui::SliderFloat("Zoom", &zoomValue, 15.0f, 60.0f)) { // Adjust the range as needed
-                        MemoryManipulation("zoom", zoomValue);
-                    }
-                } else if (prevZoomState) {
-                    // Reset zoom value to 15.0f when checkbox is unchecked
-                    zoomValue = 15.0f;
-                    MemoryManipulation("zoom", zoomValue);
-                }
-
-                prevZoomState = optionZoom; // Update previous state
-
-                // Check if the checkbox is checked
-                ImGui::BeginDisabled(!featureFov);
-                if (ImGui::Checkbox("Field of View", &optionFov)) {
-                    if (!optionFov) {
-                        // If the checkbox is unchecked, reset the FOV value
-                        MemoryManipulation("fov", 0.01745329238f);
-                    }
-                }
-                ImGui::EndDisabled();
-                ImGui::SameLine();
-
-                // Only allow users to set the hotkey if the checkbox is checked
-                if (optionFov) {
-                    std::string buttonLabel;
-                    if (waitingForHotkey) {
-                        buttonLabel = "Press any key...";
-                    } else if (userDefinedHotkey == 0) {
-                        buttonLabel = "Set Hotkey";
-                    } else {
-                        buttonLabel = "Hotkey: " + GetKeyName(userDefinedHotkey);
-                    }
-
-                    if (ImGui::Button(buttonLabel.c_str())) {
-                        waitingForHotkey = true;
-                    }
-
-                    if (waitingForHotkey) {
-                        for (int key = 0x08; key <= 0xFF; key++) {
-                            if (GetAsyncKeyState(key) & 0x8000) {
-                                userDefinedHotkey = key;
-                                waitingForHotkey = false;
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                if (!featureFov) {
-                    ImGui::SameLine();
-                    ShowLicenseMarker();
-                }  
+                ShowViewWindow(show_view_window, optionZoom, optionFov, featureFov, waitingForHotkey, userDefinedHotkey); 
             } else if (show_movement_window) {
 
                 ShowMovementWindow(show_movement_window);
