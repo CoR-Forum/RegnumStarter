298f5464c4cb43ba8e0fc467c2a2f5cc7b328344
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-25 21:56:37 +0200

    Removed Loadingscreen

--- ui/Login/Login.cpp
+++ ui/Login/Login.cpp
@@ -1,7 +1,7 @@
 #include "Login.h"
 #include "../WindowStates.h" // Include the header file where the window state variables are declared
 
-void ShowLoginWindow(bool& show_login_window, bool& show_loading_screen, std::string& statusMessage, bool& loginSuccess, bool& show_main_window, ImVec4 textColor) {
+void ShowLoginWindow(bool& show_login_window, std::string& statusMessage, bool& loginSuccess, bool& show_main_window, ImVec4 textColor) {
     static bool settingsWindowIsOpen = true;
     ImGui::Begin("Login", &settingsWindowIsOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
     
@@ -17,17 +17,11 @@ void ShowLoginWindow(bool& show_login_window, bool& show_loading_screen, std::st
     ImGui::InputText("Password", password, IM_ARRAYSIZE(password), ImGuiInputTextFlags_Password);
     
     if (ImGui::Button("Login")) {
-        show_loading_screen = true;
         statusMessage = "Logging in...";
         loginSuccess = false;
         show_login_window = false; // Hide the login window
 
         std::thread loginThread([&]() {
-            for (int i = 0; i <= 100; ++i) {
-                std::this_thread::sleep_for(std::chrono::milliseconds(10)); // Simulate progress over 1s
-                UpdateProgressBar(i / 100.0f);
-            }
-
             loginSuccess = Login(username, password);
             if (loginSuccess) {
                 Log("Login successful");
@@ -41,11 +35,11 @@ void ShowLoginWindow(bool& show_login_window, bool& show_loading_screen, std::st
                 Log("Login failed");
                 show_login_window = true; // Show the login window again if login fails
             }
-            show_loading_screen = false;
         });
 
         loginThread.detach();
     }
+    
     if (ImGui::Button("Register")) {
         show_login_window = false;
         show_register_window = true;
