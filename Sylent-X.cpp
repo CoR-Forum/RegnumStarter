7620261f793166ecc6f8f063ba4ac499846e8a11
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-25 21:39:59 +0200

    Refactor code to include Register window in Sylent-X.cpp

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -21,6 +21,7 @@
 #include "ui/PasswordResetWindow.cpp"
 #include "ui/Rainbow/UpdateRainbowColor.cpp"
 #include "ui/Login/Login.cpp"
+#include "ui/Register/Register.cpp"
 
 #pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "urlmon.lib")
@@ -221,38 +222,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
             }
 
             if (show_register_window) {
-                static bool registerWindowIsOpen = true;
-                ImGui::Begin("Register", &registerWindowIsOpen, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_AlwaysAutoResize);
-
-                if (!registerWindowIsOpen) {
-                    SaveSettings();
-                    PostQuitMessage(0);
-                }
-
-                static char regUsername[128] = "";
-                static char regPassword[128] = "";
-                static char regEmail[128] = "";
-
-                ImGui::InputText("Username", regUsername, IM_ARRAYSIZE(regUsername));
-                ImGui::InputText("Password", regPassword, IM_ARRAYSIZE(regPassword), ImGuiInputTextFlags_Password);
-                ImGui::InputText("Email", regEmail, IM_ARRAYSIZE(regEmail));
-
-                if (ImGui::Button("Register")) {
-                    RegisterUser(regUsername, regEmail, regPassword);
-                    show_register_window = false;
-                    show_login_window = true;
-                }
-
-                if (ImGui::Button("Back to Login")) {
-                    show_register_window = false;
-                    show_login_window = true;
-                }
-
-                if (ImGui::Button("Exit")) {
-                    done = true;
-                }
-
-                ImGui::End();
+                ShowRegisterWindow(show_register_window, show_login_window, done);
             }
 
             if (show_forgot_password_window) {
