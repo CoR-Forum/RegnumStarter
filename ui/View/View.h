50b661dc77b33e9187b040c187b577e72a1ed5da
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-10-26 01:34:15 +0200

    Refactor View.h: Remove unused code and improve readability

--- ui/View/View.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "../../libs/imgui/imgui.h"
-
-void ShowViewWindow(bool& show_view_window, bool& optionZoom, bool& optionFov, bool& featureFov, bool& waitingForHotkey, int& userDefinedHotkey);
-std::string GetKeyName(int virtualKey);
