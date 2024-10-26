d01120780678427547f22c589747bb6034346456
Joshua Treudler <joshua@treudler.net>
2024-10-26 02:32:25 +0200

    Refactor AdminApiHandler.h and cleanup Utils.h

--- includes/API/AdminApiHandler.h
+++ includes/API/AdminApiHandler.h
@@ -2,22 +2,23 @@
 
 #include "../Utils.h"
 #include "../InternetUtils/InternetUtils.h"
+#include "../md5/md5.h"
 #include <stdexcept>
 #include <sstream>
 #include <regex>
-#include "../md5/md5.h"
+#include <string>
 
+// Forward declaration
 extern HWND hwnd;
 
-// global variables for license key generation
-std::string generated_license_key;
+// Global variables for license key generation
+extern std::string generated_license_key;
+extern std::string GetAllLicensesRawJson;
+extern std::string GetAllUsersRawJson;
 
-std::string GetAllLicensesRawJson;
-std::string GetAllUsersRawJson;
-
+// Function declarations
 void ModifyGlobalSettings(const std::string& settingName, const std::string& settingValue);
 void GetAllLicenses();
-
 void DisplayUsersTable();
 void ToggleUserBan(int userId);
 void ToggleUserAdmin(int userId);
