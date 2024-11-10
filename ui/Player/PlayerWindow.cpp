410157f0db7cbd18211a4682339390fe24422fb2
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-11-10 06:16:43 +0100

    Update ShowPlayerWindow to display message when ROClientGame is not running

--- ui/Player/PlayerWindow.cpp
+++ ui/Player/PlayerWindow.cpp
@@ -10,15 +10,18 @@ static bool prevCharState = false; // Track previous state of the checkbox
 bool disableCheckboxes = (sylentx_status == "Detected");
 
 void ShowPlayerWindow(bool& show_player_window, bool& optionCharacter) {
+    static float prevCharValue = CharValue; // Track previous value of the slider
     if (show_player_window) {
-        /*if (IsProcessOpen("ROClientGame.exe")) {
+        if (IsProcessOpen("ROClientGame.exe")) {
             std::vector<float> values = ReadMemoryValues({"posx", "posy", "posz"});
             if (values.size() == 3) {
                 ImGui::Text("Position - X: %.2f, Y: %.2f, Z: %.2f", values[0], values[1], values[2]);
             } else {
                 ImGui::Text("Failed to read position values.");
             }
-        } */
+        } else {
+            ImGui::Text("Ragnarok Online is not running.");
+        }
         ImGui::Spacing();
         ImGui::SeparatorText("TrollOptions");
         ImGui::Spacing();
