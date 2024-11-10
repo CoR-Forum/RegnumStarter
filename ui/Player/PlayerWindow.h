2e25c6944088d8bc9732aea104cd8055cfcbc278
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-11-10 06:05:08 +0100

    Update ShowPlayerWindow function to include character option parameter

--- ui/Player/PlayerWindow.h
+++ ui/Player/PlayerWindow.h
@@ -3,4 +3,4 @@
 #include "../../libs/imgui/imgui.h"
 #include "../../includes/API/ApiHandler.h"
 
-void ShowPlayerWindow(bool& show_player_window);
+void ShowPlayerWindow(bool& show_player_window, bool& optionCharacter);
