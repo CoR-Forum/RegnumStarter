22ba19287ec7c14006ffe06725c155d2728ae57c
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-11-10 05:45:29 +0100

    Add character height adjustment options in ShowPlayerWindow function

--- ui/Player/PlayerWindow.cpp
+++ ui/Player/PlayerWindow.cpp
@@ -9,13 +9,37 @@ bool disableCheckboxes = (sylentx_status == "Detected");
 
 void ShowPlayerWindow(bool& show_player_window, bool& optionCharacter) {
     if (show_player_window) {
-        if (IsProcessOpen("ROClientGame.exe")) {
+        /*if (IsProcessOpen("ROClientGame.exe")) {
             std::vector<float> values = ReadMemoryValues({"posx", "posy", "posz"});
             if (values.size() == 3) {
                 ImGui::Text("Position - X: %.2f, Y: %.2f, Z: %.2f", values[0], values[1], values[2]);
             } else {
                 ImGui::Text("Failed to read position values.");
             }
-        }  
+        } */
+        ImGui::Spacing();
+        ImGui::SeparatorText("TrollOptions");
+        ImGui::Spacing();
+
+
+        ImGui::BeginDisabled(disableCheckboxes);
+        ImGui::Checkbox("Character Height", &optionCharacter);
+        if (optionCharacter) {
+            ImGui::SameLine();
+            if (ImGui::SliderFloat("##Character Height", &CharValue, 0.4f, 6.0f)) { // Adjust the range as needed
+                MemoryManipulation("ch", CharValue);
+                MemoryManipulation("cl", CharValue);
+                MemoryManipulation("cb", CharValue);
+            }
+        } else if (prevCharState) {
+            // Reset Value if unchecked
+            CharValue = 0.9700000286f;
+            MemoryManipulation("cl", CharValue);
+            MemoryManipulation("cb", CharValue);
+            MemoryManipulation("ch", CharValue);
+        }
+
+        prevCharState = optionCharacter; // Update previous state
+        ImGui::EndDisabled();
     }
 }
