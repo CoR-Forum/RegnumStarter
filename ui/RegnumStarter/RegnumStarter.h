01c70ce630ea37f509a471b99bf24529eb3b8ce8
Joshua Treudler <joshua@treudler.net>
2024-11-17 19:27:52 +0100

    Revert "improve RegnumSarter .h"
    This reverts commit 995b96851f9aee076659ee82f710beb3bb8c973c.

--- ui/RegnumStarter/RegnumStarter.h
+++ ui/RegnumStarter/RegnumStarter.h
@@ -3,6 +3,7 @@
 #include "../../includes/Utils.h"
 #include "../../libs/imgui/imgui.h"
 #include "../../libs/imgui/imfilebrowser.h"
+#include "../../includes/Utils.h"
 #include "../../includes/API/ApiHandler.h"
 #include <vector>
 #include <string>
@@ -14,10 +15,8 @@ void ShowRegnumStarter(bool& show_RegnumStarter);
 void runRoClientGame(const std::string& regnumLoginUser, const std::string& regnumLoginPassword);
 
 // Define the checkbox states
-namespace RegnumSettings {
-    inline bool enableMusic = true;
-    inline bool enableSoundEffects = true;
-    inline bool showLoadingScreen = true;
-    inline bool showIntro = true;
-    inline float soundVolume = 0.5f;
-}
+bool enableMusic = true;
+bool enableSoundEffects = true;
+bool showLoadingScreen = true;
+bool ShowIntro = true;
+float soundVolume = 0.5f;
