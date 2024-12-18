b8bd30943f8d4595d7c0c43bce891dc5d56112e6
Shaiko <87884051+Shaiko35@users.noreply.github.com>
2024-12-18 15:47:55 +0100

    Child fix

--- Sylent-X.cpp
+++ Sylent-X.cpp
@@ -240,7 +240,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                 ImVec2 buttonSize = ImVec2(120, 30);
 
                 // Create a child window for the navigation buttons
-                ImGui::BeginChild("Navigation", ImVec2(140, 0), true);
+                ImGui::BeginChild("Navigation", ImVec2(130, 0), true);
 
                 // Calculate the padding to center the buttons
                 float childWidth = ImGui::GetWindowWidth();
@@ -413,7 +413,7 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
                     ImGui::SameLine();
                     if (ImGui::Button(ICON_FA_EXTERNAL_LINK "##Chat")) {
                         show_chat_window = true;
-                    }
+                    }   
                 }
                 ImGui::EndChild();
                 ImGui::End();
